cef01ea25c75628e2b2c8d117d63ef606d0f9290
natias <natias@natias.de>
2021-05-22 20:03:31 +0200

    libraries

---
+++ FeelUI/Libraries/LibAnimFadeInOut-1.0/LibAnimFadeInOut-1.0.lua
@@ -0,0 +1,160 @@
+-- Call Interface
+local UI, Settings, Assets, Language = select(2, ...):Call()
+
+-- Lib Globals
+local select = select
+local unpack = unpack
+local next = next
+
+-- Locals
+local FADEFRAMES, FADEMANAGER = {}, CreateFrame('FRAME')
+FADEMANAGER.delay = 0
+
+function UI:UIFrameFade_OnUpdate(elapsed)
+	FADEMANAGER.timer = (FADEMANAGER.timer or 0) + elapsed
+
+	if (FADEMANAGER.timer > FADEMANAGER.delay) then
+		FADEMANAGER.timer = 0
+
+		for frame, info in next, FADEFRAMES do
+			if (frame:IsVisible()) then
+				info.fadeTimer = (info.fadeTimer or 0) + (elapsed + FADEMANAGER.delay)
+			else
+				info.fadeTimer = info.timeToFade + 1
+			end
+
+			if (info.fadeTimer < info.timeToFade) then
+				if (info.mode == 'IN') then
+					frame:SetAlpha((info.fadeTimer / info.timeToFade) * info.diffAlpha + info.startAlpha)
+				else
+					frame:SetAlpha(((info.timeToFade - info.fadeTimer) / info.timeToFade) * info.diffAlpha + info.endAlpha)
+				end
+			else
+				frame:SetAlpha(info.endAlpha)
+
+				if (info.fadeHoldTime and info.fadeHoldTime > 0)  then
+					info.fadeHoldTime = info.fadeHoldTime - elapsed
+				else
+					UI:UIFrameFadeRemoveFrame(frame)
+
+					if (info.finishedFunc) then
+						if (info.finishedArgs) then
+							info.finishedFunc(unpack(info.finishedArgs))
+						else
+							info.finishedFunc(info.finishedArg1, info.finishedArg2, info.finishedArg3, info.finishedArg4, info.finishedArg5)
+						end
+
+						if (not info.finishedFuncKeep) then
+							info.finishedFunc = nil
+						end
+					end
+				end
+			end
+		end
+
+		if (not next(FADEFRAMES)) then
+			FADEMANAGER:SetScript('OnUpdate', nil)
+		end
+	end
+end
+
+function UI:UIFrameFade(frame, info)
+	if (not frame or frame:IsForbidden()) then
+		return
+	end
+
+	frame.fadeInfo = info
+
+	if (not info.mode) then
+		info.mode = 'IN'
+	end
+
+	if (info.mode == 'IN') then
+		if (not info.startAlpha) then
+			info.startAlpha = 0
+		end
+
+		if (not info.endAlpha) then
+			info.endAlpha = 1
+		end
+
+		if (not info.diffAlpha) then
+			info.diffAlpha = info.endAlpha - info.startAlpha
+		end
+	else
+		if (not info.startAlpha) then
+			info.startAlpha = 1
+		end
+
+		if (not info.endAlpha) then
+			info.endAlpha = 0
+		end
+
+		if (not info.diffAlpha) then
+			info.diffAlpha = info.startAlpha - info.endAlpha
+		end
+	end
+
+	frame:SetAlpha(info.startAlpha)
+
+	if (not frame:IsProtected()) then
+		frame:Show()
+	end
+
+	if (not FADEFRAMES[frame]) then
+		FADEFRAMES[frame] = info
+		FADEMANAGER:SetScript('OnUpdate', UI.UIFrameFade_OnUpdate)
+	else
+		FADEFRAMES[frame] = info
+	end
+end
+
+function UI:UIFrameFadeIn(frame, timeToFade, startAlpha, endAlpha)
+	if (not frame or frame:IsForbidden()) then
+		return
+	end
+
+	if (frame.FadeObject) then
+		frame.FadeObject.fadeTimer = nil
+	else
+		frame.FadeObject = {}
+	end
+
+	frame.FadeObject.mode = 'IN'
+	frame.FadeObject.timeToFade = timeToFade
+	frame.FadeObject.startAlpha = startAlpha
+	frame.FadeObject.endAlpha = endAlpha
+	frame.FadeObject.diffAlpha = endAlpha - startAlpha
+
+	UI:UIFrameFade(frame, frame.FadeObject)
+end
+
+function UI:UIFrameFadeOut(frame, timeToFade, startAlpha, endAlpha)
+	if (not frame or frame:IsForbidden()) then
+		return
+	end
+
+	if (frame.FadeObject) then
+		frame.FadeObject.fadeTimer = nil
+	else
+		frame.FadeObject = {}
+	end
+
+	frame.FadeObject.mode = 'OUT'
+	frame.FadeObject.timeToFade = timeToFade
+	frame.FadeObject.startAlpha = startAlpha
+	frame.FadeObject.endAlpha = endAlpha
+	frame.FadeObject.diffAlpha = startAlpha - endAlpha
+
+	UI:UIFrameFade(frame, frame.FadeObject)
+end
+
+function UI:UIFrameFadeRemoveFrame(frame)
+	if (frame and FADEFRAMES[frame]) then
+		if (frame.FadeObject) then
+			frame.FadeObject.fadeTimer = nil
+		end
+
+		FADEFRAMES[frame] = nil
+	end
+end
---
+++ FeelUI/Libraries/LibAnimGainLoss-1.0/LibAnimGainLoss-1.0.lua
@@ -0,0 +1,187 @@
+-- Call Interface
+local UI, Settings, Assets, Language = select(2, ...):Call()
+
+-- Lib Globals
+local min = min
+local max = max
+local m_abs = _G.math.abs
+local m_max = _G.math.max
+local m_min = _G.math.min
+
+local clamp = function(v, min, max)
+	return m_min(max or 1, m_max(min or 0, v))
+end
+
+local attachGainToVerticalBar = function(self, object, prev, max)
+	local offset = object:GetHeight() * (1 - clamp(prev / max))
+
+	self:SetPoint("BOTTOMLEFT", object, "TOPLEFT", 0, -offset)
+	self:SetPoint("BOTTOMRIGHT", object, "TOPRIGHT", 0, -offset)
+end
+
+local attachLossToVerticalBar = function(self, object, prev, max)
+	local offset = object:GetHeight() * (1 - clamp(prev / max))
+
+	self:SetPoint("TOPLEFT", object, "TOPLEFT", 0, -offset)
+	self:SetPoint("TOPRIGHT", object, "TOPRIGHT", 0, -offset)
+end
+
+local attachGainToHorizontalBar = function(self, object, prev, max)
+	local offset = object:GetWidth() * (1 - clamp(prev / max))
+
+	self:SetPoint("TOPLEFT", object, "TOPRIGHT", -offset, 0)
+	self:SetPoint("BOTTOMLEFT", object, "BOTTOMRIGHT", -offset, 0)
+end
+
+local attachLossToHorizontalBar = function(self, object, prev, max)
+	local offset = object:GetWidth() * (1 - clamp(prev / max))
+
+	self:SetPoint("TOPRIGHT", object, "TOPRIGHT", -offset, 0)
+	self:SetPoint("BOTTOMRIGHT", object, "BOTTOMRIGHT", -offset, 0)
+end
+
+local update = function(self, cur, max, condition)
+	if (max and max ~= 0 and (condition == nil or condition)) then
+		local prev = (self._prev or cur) * max / (self._max or max)
+		local diff = cur - prev
+
+		if (m_abs(diff) / max < self.threshold) then
+			diff = 0
+		end
+
+		if (diff > 0) then
+			if (self.Gain and self.Gain:GetAlpha() == 0) then
+				self.Gain:SetAlpha(1)
+
+				if (self.orientation == "VERTICAL") then
+					attachGainToVerticalBar(self.Gain, self.__owner, prev, max)
+				else
+					attachGainToHorizontalBar(self.Gain, self.__owner, prev, max)
+				end
+
+				self.Gain.FadeOut:Play()
+			end
+		elseif (diff < 0) then
+			if (self.Gain) then
+				self.Gain.FadeOut:Stop()
+				self.Gain:SetAlpha(0)
+			end
+
+			if (self.Loss) then
+				if (self.Loss:GetAlpha() <= 0.33) then
+					self.Loss:SetAlpha(1)
+
+					if (self.orientation == "VERTICAL") then
+						attachLossToVerticalBar(self.Loss, self.__owner, prev, max)
+					else
+						attachLossToHorizontalBar(self.Loss, self.__owner, prev, max)
+					end
+
+					self.Loss.FadeOut:Restart()
+				elseif (self.Loss.FadeOut.Alpha:IsDelaying() or self.Loss:GetAlpha() >= 0.66) then
+					self.Loss.FadeOut:Restart()
+				end
+			end
+		end
+	else
+		if (self.Gain) then
+			self.Gain.FadeOut:Stop()
+			self.Gain:SetAlpha(0)
+		end
+
+		if (self.Loss) then
+			self.Loss.FadeOut:Stop()
+			self.Loss:SetAlpha(0)
+		end
+	end
+
+	if (max and max ~= 0) then
+		self._prev = cur
+		self._max = max
+	else
+		self._prev = nil
+		self._max = nil
+	end
+end
+
+local updateColors = function(self)
+	self.Gain_:SetColorTexture(0, 0.8, 0, 0.5)
+	self.Loss_:SetColorTexture(0.8, 0, 0, 0.5)
+end
+
+local updatePoints = function(self, orientation)
+	orientation = orientation or "HORIZONTAL"
+
+	if (orientation == "HORIZONTAL") then
+		self.Gain_:ClearAllPoints()
+		self.Gain_:SetPoint("TOPRIGHT", self.__owner:GetStatusBarTexture(), "TOPRIGHT", 0, 0)
+		self.Gain_:SetPoint("BOTTOMRIGHT", self.__owner:GetStatusBarTexture(), "BOTTOMRIGHT", 0, 0)
+
+		self.Loss_:ClearAllPoints()
+		self.Loss_:SetPoint("TOPLEFT", self.__owner:GetStatusBarTexture(), "TOPRIGHT", 0, 0)
+		self.Loss_:SetPoint("BOTTOMLEFT", self.__owner:GetStatusBarTexture(), "BOTTOMRIGHT", 0, 0)
+	else
+		self.Gain_:ClearAllPoints()
+		self.Gain_:SetPoint("TOPLEFT", self.__owner:GetStatusBarTexture(), "TOPLEFT", 0, 0)
+		self.Gain_:SetPoint("TOPRIGHT", self.__owner:GetStatusBarTexture(), "TOPRIGHT", 0, 0)
+
+		self.Loss_:ClearAllPoints()
+		self.Loss_:SetPoint("BOTTOMLEFT", self.__owner:GetStatusBarTexture(), "TOPLEFT", 0, 0)
+		self.Loss_:SetPoint("BOTTOMRIGHT", self.__owner:GetStatusBarTexture(), "TOPRIGHT", 0, 0)
+	end
+
+	self.orientation = orientation
+end
+
+local updateThreshold = function(self, value)
+	self.threshold = value or 0.01
+end
+
+function UI:CreateGainLossIndicators(object)
+	local Texture = Assets.Textures.Normal
+
+	local gainTexture = object:CreateTexture(nil, "OVERLAY", nil, 1)
+	gainTexture:SetTexture(Texture)
+	gainTexture:SetAlpha(0)
+
+	local ag = gainTexture:CreateAnimationGroup()
+	ag:SetToFinalAlpha(true)
+	gainTexture.FadeOut = ag
+
+	local anim = ag:CreateAnimation("Alpha")
+	anim:SetOrder(1)
+	anim:SetFromAlpha(1)
+	anim:SetToAlpha(0)
+	anim:SetStartDelay(0.25)
+	anim:SetDuration(0.1)
+	ag.Alpha = anim
+
+	local lossTexture = object:CreateTexture(nil, "OVERLAY", nil, 1)
+	lossTexture:SetTexture(Texture)
+	lossTexture:SetAlpha(0)
+
+	ag = lossTexture:CreateAnimationGroup()
+	ag:SetToFinalAlpha(true)
+	lossTexture.FadeOut = ag
+
+	anim = ag:CreateAnimation("Alpha")
+	anim:SetOrder(1)
+	anim:SetFromAlpha(1)
+	anim:SetToAlpha(0)
+	anim:SetStartDelay(0.25)
+	anim:SetDuration(0.1)
+	ag.Alpha = anim
+
+	return {
+		__owner = object,
+		threshold = 0.01,
+		Gain = gainTexture,
+		Gain_ = gainTexture,
+		Loss = lossTexture,
+		Loss_ = lossTexture,
+		Update = update,
+		UpdateColors = updateColors,
+		UpdatePoints = updatePoints,
+		UpdateThreshold = updateThreshold,
+	}
+end
